d1995c7dcca778d3eabc1e6301d3bc35c166da1e
kelly r maass <kmaass77627@gmail.com>
2019-10-25 01:55:46 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,8 +12,8 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-  while #{age} <= 12
-    do "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  if #{age} <= 12
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end 
 end
 end
